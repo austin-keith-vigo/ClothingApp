9c2bf505a4c79ac7d6445285b47366af8e948fc8
Austin Vigo <avigo@uci.edu>
2019-08-01 18:27:42 -0700

    Set up the project completely
    Set up the project and is ready for developing

---
+++ ClothingApp/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, AppRegistry } from 'react-native';
+
+const App = () => {
+  return(
+    <View>
+      <Text>
+        Hello
+      </Text>
+    </View>
+  );
+};
+
+AppRegistry.registerComponent('ClothingApp', () => App);
