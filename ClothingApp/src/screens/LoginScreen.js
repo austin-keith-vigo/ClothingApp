f50a308e9fff35f0c82e48492635524d46d144f5
Austin Vigo <avigo@uci.edu>
2019-08-07 16:37:12 -0700

    Login is working
    can login with firebase and navigate to the home screen when it is done loggin in

--- ClothingApp/src/screens/LoginScreen.js
+++ ClothingApp/src/screens/LoginScreen.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
-import LoginForm from './../components/LoginForm'
+import LoginForm from './../components/LoginForm';
+import firebase from 'firebase';
 
 class LoginScreen extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyC0JR6pVP6v0JcX0NwCm5f-ZsEYQe-Ydbk",
+      authDomain: "my-closet-c4c42.firebaseapp.com",
+      databaseURL: "https://my-closet-c4c42.firebaseio.com",
+      projectId: "my-closet-c4c42",
+      storageBucket: "",
+      messagingSenderId: "870899532598",
+      appId: "1:870899532598:web:03be0160a5aa15e5"
+    });
+  }
 
   static navigationOptions = {
     title: 'Home',
@@ -15,12 +28,13 @@ class LoginScreen extends Component {
   render() {
     return(
       <View>
+        <LoginForm navigationProp={this.props.navigation}/>
         <Button
           onPress={() => {
             this.props.navigation.navigate('Home')
           }}
           title="To Home"
-          />
+        />
       </View>
     );
   }
