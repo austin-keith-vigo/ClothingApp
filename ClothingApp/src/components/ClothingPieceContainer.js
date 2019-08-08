b6fed3f7cb60e907e11ac5b9ed803b2240d73233
Austin Vigo <avigo@uci.edu>
2019-08-08 15:44:26 -0700

    wraps around array
    it now wraps around an array

--- ClothingApp/src/components/ClothingPieceContainer.js
+++ ClothingApp/src/components/ClothingPieceContainer.js
@@ -4,7 +4,32 @@ import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 
 class ClothingPieceContainer extends Component {
 
-  state = { currentPiece:this.props.pieces[0] }
+  state = { currentPiece: this.props.pieces[0] }
+  currentPieceIndex = 0;
+
+  nextPiece() {
+    if (this.currentPieceIndex == this.props.pieces.length - 1){
+      this.currentPieceIndex = 0;
+    }
+    else {
+      ++this.currentPieceIndex;
+    }
+
+    console.log(this.currentPieceIndex);
+    this.setState({ currentPiece: this.props.pieces[this.currentPieceIndex] });
+  }
+
+  previousPiece() {
+    if (this.currentPieceIndex == 0) {
+      this.currentPieceIndex = this.props.pieces.length - 1;
+    }
+    else {
+      --this.currentPieceIndex;
+    }
+
+    console.log(this.currentPieceIndex);
+    this.setState({ currentPiece: this.props.pieces[this.currentPieceIndex] });
+  }
 
   viewStyle = {
     backgroundColor: "#800000",
@@ -16,8 +41,8 @@ class ClothingPieceContainer extends Component {
     return(
         <View style={this.viewStyle}>
           <GestureRecognizer
-            onSwipeRight={() => this.setState({ currentPiece:this.props.pieces[1]}) }
-            onSwipeLeft={() => this.setState({ currentPiece:this.props.pieces[1]}) }
+            onSwipeRight={() => this.previousPiece() }
+            onSwipeLeft={() => this.nextPiece() }
             style={{
               flex: 1
             }}
