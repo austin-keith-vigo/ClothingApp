0ffd90cc1fb48447d8f040d5e6812769bafb35eb
Austin Vigo <avigo@uci.edu>
2019-08-25 13:30:59 -0700

    dfads
    fdas

--- ClothingApp/src/screens/SplashScreen.js
+++ ClothingApp/src/screens/SplashScreen.js
@@ -7,26 +7,8 @@ import {
 import firebase from 'firebase';
 
 class SplashScreen extends Component{
-  //
-  // constructor(props){
-  //   super(props);
-  //   firebase.initializeApp({
-  //     apiKey: "AIzaSyC0JR6pVP6v0JcX0NwCm5f-ZsEYQe-Ydbk",
-  //     authDomain: "my-closet-c4c42.firebaseapp.com",
-  //     databaseURL: "https://my-closet-c4c42.firebaseio.com",
-  //     projectId: "my-closet-c4c42",
-  //     storageBucket: "gs://my-closet-c4c42.appspot.com",
-  //     messagingSenderId: "870899532598",
-  //     appId: "1:870899532598:web:03be0160a5aa15e5"
-  //   })
-  //   .then(()=>{
-  //     console.log('it worked')
-  //   })
-  //   .catch((error)=>{
-  //     console.log(error);
-  //   });
-  // }
 
+  //Initialize firebase when the splash screen loads in
   componentWillMount(){
     firebase.initializeApp({
       apiKey: "AIzaSyC0JR6pVP6v0JcX0NwCm5f-ZsEYQe-Ydbk",
